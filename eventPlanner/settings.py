f227574791fadb422a458e1c51b6a3714ebd0423
BIBIN KUNJUMON <104210649+bibinkunjumon2020@users.noreply.github.com>
2022-11-16 17:55:23 +0530

    Update settings.py

--- eventPlanner/settings.py
+++ eventPlanner/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^b7&r&!*v3^rwc$dh#)=ip348#shz29@b*mep!j@8007%mttz2'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
